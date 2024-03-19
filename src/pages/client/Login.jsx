3db3bf4fc5df01c7554cae8831762aef9a01a998
majestymewtwo <majestymewtwo@gmail.com>
2024-03-19 12:19:35 +0530

    outlet added

--- src/pages/client/Login.jsx
+++ src/pages/client/Login.jsx
@@ -3,13 +3,8 @@ import Header from "../../components/Header";
 
 const Login = () => {
   return (
-    <div className="flex flex-col h-svh">
-      <Header />
-      {/* Main content goes here */}
-      <div className="h-[90%] flex justify-center items-center">
-        <div className="bg-slate-300 w-[25%] h-[75%]">Hi</div>
-      </div>
-      <Footer />
+    <div className='h-[90%] flex justify-center items-center'>
+      <div className='bg-slate-300 w-[25%] h-[75%]'>Hi</div>
     </div>
   );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,15 +1,22 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Home from "@/pages/client/Home";
 import Login from "@/pages/client/Login";
+import Client from "../layout/Client";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Client />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
   {
-    path: "/login",
-    element: <Login />,
+    path: "*",
+    element: <Navigate to='/' />,
   },
 ]);
 
