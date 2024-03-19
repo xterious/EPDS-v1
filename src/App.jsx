10a477e2c361e1348b824fb463e376bb42231880
majestymewtwo <majestymewtwo@gmail.com>
2024-03-19 12:13:06 +0530

    router wrapped inside app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/Routes";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import router from "./routes/Routes";
-import { RouterProvider } from "react-router-dom";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
