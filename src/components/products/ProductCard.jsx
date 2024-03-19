55a09a1c0c8b7c2b2bf1066ddca96af86661f507
majestymewtwo <majestymewtwo@gmail.com>
2024-03-19 22:29:48 +0530

    product card changed

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,29 +1,23 @@
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-const ProductCard = ({ id, imageUrl, name, price }) => {
+const ProductCard = ({ id, imageUrl, name, price, scale }) => {
   return (
-    <div key={id} className='bg-white p-4 flex flex-col items-center shadow-lg'>
-      <img src={imageUrl} alt={name} className='w-32 h-32 object-cover' />
-      <h3 className='mt-2 text-lg font-semibold'>{name}</h3>
-      <div className='flex justify-between w-[60%] items-center'>
-        <p className='mt-1'>₹{price}</p>
-        {
-          <div className='mt-auto flex items-center'>
-            <button className='bg-none border border-red-500 text-red-500 py-1 px-2 rounded-l hover:bg-red-500 hover:text-white transition duration-300'>
-              -
-            </button>
-            <span className='px-2'>{id}</span>
-            <button className='bg-none border border-red-500 text-red-500 py-1 px-2 rounded-r hover:bg-red-500 hover:text-white transition duration-300'>
-              +
-            </button>
-          </div>
-        }
-        {
-          <button className='px-3 py-1 text-red-500 rounded-lg bg-none border border-red-500 hover:bg-red-500 hover:text-white transition duration-300'>
-            Add
-          </button>
-        }
+    <div
+      key={id}
+      className='bg-white p-4 flex flex-col items-center shadow-lg border rounded-md space-y-4 h-fit'>
+      <img src={imageUrl} alt={name} className='w-full h-32 object-cover' />
+      <div className='flex items-center justify-between w-[70%]'>
+        <h3 className='mt-2 text-lg font-semibold capitalize'>{name}</h3>
+        <p className=''>
+          ₹{price} per <span className='capitalize'>{scale}</span>
+        </p>
       </div>
+      <button className='px-3 py-1 w-[60%] flex items-center justify-around gap-1 text-red-500 rounded-lg bg-none border border-red-500 hover:bg-red-500 hover:text-white transition duration-300'>
+        <p>Add to Cart</p>
+        <FontAwesomeIcon icon={faCartPlus} />
+      </button>
     </div>
   );
 };
--- src/pages/client/Products.jsx
+++ src/pages/client/Products.jsx
@@ -1,81 +1,46 @@
 import TopNavbar from "@/components/client/Navbar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CartModal from "@/components/products/CartModal";
 import ProductCard from "@/components/products/ProductCard";
-
-// Sample product data
-const products = [
-  {
-    id: 1,
-    name: "Organic Bananas",
-    price: "0.99",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 2,
-    name: "Fresh Strawberries",
-    price: "1.49",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 3,
-    name: "Fresh Mangoes",
-    price: "2.49",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 4,
-    name: "Avocado",
-    price: "1.99",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 5,
-    name: "Grapes",
-    price: "0.79",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 6,
-    name: "Watermelon",
-    price: "3.99",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 7,
-    name: "Pineapple",
-    price: "2.99",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 8,
-    name: "Oranges",
-    price: "0.69",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 9,
-    name: "Apples",
-    price: "0.89",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-  {
-    id: 10,
-    name: "Blueberries",
-    price: "1.99",
-    imageUrl: "https://via.placeholder.com/150",
-  },
-];
+import axios from "axios";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    axios
+      .get(`${import.meta.env.VITE_APP_API_URL}/product/all`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token-client"),
+        },
+      })
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <TopNavbar />
       <CartModal />
       <div className='bg-gray-100 p-4'>
-        <div className='max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-          {products.map((product) => (
-            <ProductCard />
+        <div className='max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 min-h-[60vh]'>
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              id={product.id}
+              name={product.name}
+              imageUrl={product.image}
+              scale={product.scale}
+              price={product.price}
+            />
           ))}
         </div>
       </div>
