832baa7a0339e8a12c275ec6f43e8acf2996d6d7
majestymewtwo <majestymewtwo@gmail.com>
2024-03-19 21:47:31 +0530

    added demo note to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 const Header = () => {
   return (
     <>
-      <div className='w-full bg-slate-800 flex justify-end px-3 py-1'>
+      <div className='w-full bg-slate-800 flex items-center justify-between px-3 py-1'>
+        <p className='text-slate-50 text-[9px]'>
+          Note : This is for demonstrating purpose only
+        </p>
         <ul className='flex space-x-5 text-xs lg:text-sm lg:w-[20%] text-white font-medium'>
           <li>English</li>
           <li>தமிழ்</li>
