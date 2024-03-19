b8c12b50c73e513522a038bb018fe77eebeb662c
majestymewtwo <majestymewtwo@gmail.com>
2024-03-19 14:33:06 +0530

    added hero section

--- src/pages/client/Home.jsx
+++ src/pages/client/Home.jsx
@@ -1,11 +1,13 @@
 import AuthForm from "@/components/home/Form";
 import HelpInfo from "@/components/home/HelpInfo";
+import Hero from "@/components/home/Hero";
 import Locations from "@/components/home/Locations";
 import UserDetails from "@/components/home/UserDetails";
 
 const Home = () => {
   return (
     <div className='p-4 space-y-4'>
+      <Hero />
       <div className='flex flex-col lg:flex-row items-center gap-8'>
         <Locations />
         <UserDetails />
