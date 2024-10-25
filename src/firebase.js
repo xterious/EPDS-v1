28e8e3bfc7c93b613db61a437145dd45c000ad1a
majestymewtwo <majestymewtwo@gmail.com>
2024-10-25 11:47:44 +0530

    removed measurement id

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,6 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
